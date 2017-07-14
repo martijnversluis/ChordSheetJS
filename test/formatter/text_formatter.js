613c88a936a996ce3543c6135893f012938afe12
Martijn Versluis <martijnversluis@users.noreply.github.com>
2017-07-14 21:40:10 +0200

    Export song fixture to separate file

--- test/formatter/text_formatter.js
+++ test/formatter/text_formatter.js
@@ -1,48 +1,10 @@
 import expect from 'expect';
 import '../matchers';
-import { createSong, createLine, createItem, createTag } from '../utilities';
 import TextFormatter from '../../src/formatter/text_formatter';
+import song from '../fixtures/song';
 
 describe('TextFormatter', () => {
   it('formats a song to a text chord sheet correctly', () => {
-    // Mimic the following chord sheet:
-    //
-    // {title: Let it be}
-    // {Chorus}
-    //
-    // Let it [Am]be, let it [C/G]be, let it [F]be, let it [C]be
-    // [C]Whisper words of [G]wisdom, let it [F]be [C/E] [Dm] [C]
-
-    const song = createSong([
-      createLine([]),
-
-      createLine([
-        createTag('Chorus', '')
-      ]),
-
-      createLine([]),
-
-      createLine([
-        createItem('', 'Let it '),
-        createItem('Am', 'be, let it '),
-        createItem('C/G', 'be, let it '),
-        createItem('F', 'be, let it '),
-        createItem('C', 'be')
-      ]),
-
-      createLine([
-        createItem('C', 'Whisper words of '),
-        createItem('G', 'wisdom, let it '),
-        createItem('F', 'be '),
-        createItem('C/E', ' '),
-        createItem('Dm', ' '),
-        createItem('C', '')
-      ])
-    ], {
-      title: 'Let it be',
-      subtitle: 'ChordSheetJS example version'
-    });
-
     const formatter = new TextFormatter();
 
     const expectedChordSheet = `
