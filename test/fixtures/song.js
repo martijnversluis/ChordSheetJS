af2fa373cedf8c17ac002ffc049ea6e68d7166b3
Martijn Versluis <martijnversluis@users.noreply.github.com>
2018-05-27 22:18:57 +0200

    Correct documentation for fixture song

--- test/fixtures/song.js
+++ test/fixtures/song.js
@@ -3,7 +3,8 @@ import { createSong, createLine, createChordLyricsPair, createTag } from '../uti
 // Mimic the following chord sheet:
 // {title: Let it be}
 // {subtitle: ChordSheetJS example version}
-// {Chorus}
+// {x_some_setting: value}
+// {comment: Bridge}
 //
 // Let it [Am]be, let it [C/G]be, let it [F]be, let it [C]be
 // [C]Whisper words of [F]wis[G]dom, let it [F]be [C/E] [Dm] [C]
