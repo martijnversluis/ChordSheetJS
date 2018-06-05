b832ce6e7902957d15f4994f6336997c454bbee2
Martijn Versluis <martijnversluis@users.noreply.github.com>
2018-06-05 17:23:21 +0200

    Add Line#isEmpty

--- test/chord_sheet/line.js
+++ test/chord_sheet/line.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 
 import Line from '../../src/chord_sheet/line';
 import ItemStub from '../cloneable_stub';
+import { createLine } from '../utilities';
 
 describe('Line', () => {
   describe('#clone', () => {
@@ -12,6 +13,25 @@ describe('Line', () => {
 
       const actualValues = clonedLine.items.map(item => item.value);
       expect(actualValues).to.eql(['foo', 'bar']);
+    });
+  });
+
+  describe('#isEmpty', () => {
+    context('when the line has items', () => {
+      it('returns false', () => {
+        const item = new ItemStub();
+        const line = createLine([item]);
+
+        expect(line.isEmpty()).to.be.false;
+      });
+    });
+
+    context('when the line has no items', () => {
+      it('returns false', () => {
+        const line = createLine([]);
+
+        expect(line.isEmpty()).to.be.true;
+      });
     });
   });
 });
