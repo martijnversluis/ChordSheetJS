d71aabda301d16ca1dfaa6a88534cd7ea27c2524
Martijn Versluis <martijnversluis@users.noreply.github.com>
2018-08-12 08:33:56 +0200

    Extract HandleBars helpers to separate file

--- src/formatter/html_div_formatter.js
+++ src/formatter/html_div_formatter.js
@@ -1,47 +1,7 @@
 import hbs from 'handlebars-inline-precompile';
-import HandleBars from 'handlebars/runtime';
 
 import HtmlFormatter from './html_formatter';
-import ChordLyricsPair from '../chord_sheet/chord_lyrics_pair';
-import Tag from '../chord_sheet/tag';
-
-const lineHasContents = (line) => {
-  return line.items.some((item) => {
-    return item instanceof ChordLyricsPair || (item instanceof Tag && item.isRenderable());
-  });
-};
-
-HandleBars.registerHelper('isChordLyricsPair', (item) => {
-  return item instanceof ChordLyricsPair;
-});
-
-HandleBars.registerHelper('isTag', (item) => {
-  return item instanceof Tag;
-});
-
-HandleBars.registerHelper('isComment', (item) => {
-  return item.name === 'comment';
-});
-
-HandleBars.registerHelper('shouldRenderLine', (line, options) => {
-  if (options.data.root.renderBlankLines) {
-    return true;
-  }
-
-  return lineHasContents(line);
-});
-
-HandleBars.registerHelper('lineHasContents', lineHasContents);
-
-HandleBars.registerHelper('lineClasses', (line) => {
-  const classes = ['row'];
-
-  if (!lineHasContents(line)) {
-    classes.push('empty-line');
-  }
-
-  return classes.join(' ');
-});
+import '../handlebars_helpers';
 
 const template = hbs`
 {{~#with song~}}
