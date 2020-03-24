e78c8203f709801b0bc2cdde69e5a934dd05adbd
ChristophWalter <ChristophWalter@users.noreply.github.com>
2020-03-24 17:04:05 +0100

    remove additional linebreak to fix tests

--- src/formatter/html_div_formatter.js
+++ src/formatter/html_div_formatter.js
@@ -20,8 +20,7 @@ const template = hbs`
             <div class="{{lineClasses line}}">
               {{~#each items as |item|~}}
                 {{~#if (isChordLyricsPair item)~}}
-                  <div class="column">
-                    <div class="chord">{{chords}}</div>
+                  <div class="column"><div class="chord">{{chords}}</div>
                     {{~#if (hasTextContents line)~}}
                       <div class="lyrics">{{lyrics}}</div>
                     {{~/if~}}
