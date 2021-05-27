b04b2e7b16706ad5afbcf348f37897453db8424e
Martijn Versluis <martijnversluis@users.noreply.github.com>
2021-05-27 11:03:26 +0200

    Add named exports for all classes
    This allows using classes as follows:
    
        import { ChordProParser } from 'chordsheetjs';
        const parser = new ChordProParser();
    
    instead of:
    
        import chordsheetjs from 'chordsheetjs';
        const parser = new chordsheetjs.ChordProParser();
    
    Resolves #317

---
+++ src/index.js
@@ -0,0 +1,12 @@
+export { default as ChordProParser } from './parser/chord_pro_parser';
+export { default as ChordSheetParser } from './parser/chord_sheet_parser';
+export { default as UltimateGuitarParser } from './parser/ultimate_guitar_parser';
+export { default as TextFormatter } from './formatter/text_formatter';
+export { default as HtmlTableFormatter } from './formatter/html_table_formatter';
+export { default as HtmlDivFormatter } from './formatter/html_div_formatter';
+export { default as ChordProFormatter } from './formatter/chord_pro_formatter';
+export { default as ChordLyricsPair } from './chord_sheet/chord_lyrics_pair';
+export { default as Line } from './chord_sheet/line';
+export { default as Song } from './chord_sheet/song';
+export { default as Tag } from './chord_sheet/tag';
+export { default as Comment } from './chord_sheet/comment';
