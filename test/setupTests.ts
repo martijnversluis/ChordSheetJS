bd2939c37f551e90efdf6b66b93797c28ce6aabd
isaiah dahl <isaiahdahl@me.com>
2022-07-04 22:13:26 -0700

    add types for extending expect for jest

---
+++ test/setupTests.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// eslint-disable-file import/no-extraneous-dependencies
+import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
