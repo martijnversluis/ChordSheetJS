f4bf3a74f06dd9d30ae35c945daaa56bded14271
Isaiah Dahl <isaiahdahl@me.com>
2022-08-11 11:59:16 -0700

    Fixes #226 Normalize Tweaks (#227)
    * consolidate normalizing logic, normalize bass from root, normalzie bass from relative major of minor root
    
    * fix grammar in doc
    
    * fixup! 6fcad88a63de4f91b1f57b97c9ba9a3787042522
    
    Co-authored-by: Martijn Versluis <martijnversluis@users.noreply.github.com>

--- src/key.ts
+++ src/key.ts
@@ -272,6 +272,12 @@ class Key {
     return this.clone();
   }
 
+  removeMinor() {
+    return this.set({
+      minor: false,
+    });
+  }
+
   normalizeEnharmonics(key: Key | string) {
     if (key) {
       const rootKeyString = Key.wrap(key).toString({ showMinor: true });
