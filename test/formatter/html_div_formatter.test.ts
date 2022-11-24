039aa11a6b0a0a8d7aca604639aebb6150d8ea68
Martijn Versluis <martijnversluis@users.noreply.github.com>
2022-11-24 21:06:44 +0100

    Refactor HTML literals with `stripHTML()` (#752)

--- test/formatter/html_div_formatter.test.ts
+++ test/formatter/html_div_formatter.test.ts
@@ -3,107 +3,110 @@ import '../matchers';
 import song from '../fixtures/song';
 import { createChordLyricsPair, createSong } from '../utilities';
 import { defaultCss, scopedCss } from '../../src/formatter/html_div_formatter';
+import { stripHTML } from '../../src/template_helpers';
 
 describe('HtmlDivFormatter', () => {
   it('formats a song to a html chord sheet correctly', () => {
     const formatter = new HtmlDivFormatter();
 
-    const expectedChordSheet = '<h1>Let it be</h1>'
-      + '<h2>ChordSheetJS example version</h2>'
-      + '<div class="chord-sheet">'
-        + '<div class="paragraph">'
-          + '<div class="row">'
-            + '<div class="column">'
-              + '<div class="chord"></div>'
-              + '<div class="lyrics">Written by: </div>'
-            + '</div>'
-            + '<div class="column">'
-              + '<div class="chord"></div>'
-              + '<div class="lyrics">John Lennon,Paul McCartney</div>'
-            + '</div>'
-          + '</div>'
-        + '</div>'
-        + '<div class="paragraph verse">'
-          + '<div class="row">'
-            + '<h3 class="label">Verse 1</h3>'
-          + '</div>'
-          + '<div class="row">'
-            + '<div class="column">'
-              + '<div class="chord"></div>'
-              + '<div class="lyrics">Let it </div>'
-            + '</div>'
-            + '<div class="column">'
-              + '<div class="chord">Am</div>'
-              + '<div class="lyrics">be, let it </div>'
-            + '</div>'
-            + '<div class="column">'
-              + '<div class="chord">C/G</div>'
-              + '<div class="lyrics">be, let it </div>'
-            + '</div>'
-            + '<div class="column">'
-              + '<div class="chord">F</div>'
-              + '<div class="lyrics">be, let it </div>'
-            + '</div>'
-            + '<div class="column">'
-              + '<div class="chord">C</div>'
-              + '<div class="lyrics">be</div>'
-            + '</div>'
-          + '</div>'
-          + '<div class="row">'
-            + '<div class="column">'
-              + '<div class="chord">D</div>'
-              + '<div class="lyrics">Whisper words of </div>'
-            + '</div>'
-            + '<div class="column">'
-              + '<div class="chord">G</div>'
-              + '<div class="lyrics">wis</div>'
-            + '</div>'
-            + '<div class="column">'
-              + '<div class="chord">A</div>'
-              + '<div class="lyrics">dom, let it </div>'
-            + '</div>'
-            + '<div class="column">'
-              + '<div class="chord">G</div>'
-              + '<div class="lyrics">be </div>'
-            + '</div>'
-            + '<div class="column">'
-              + '<div class="chord">D/F#</div>'
-              + '<div class="lyrics"> </div>'
-            + '</div>'
-            + '<div class="column">'
-              + '<div class="chord">Em</div>'
-              + '<div class="lyrics"> </div>'
-            + '</div>'
-            + '<div class="column">'
-              + '<div class="chord">D</div>'
-              + '<div class="lyrics"></div>'
-            + '</div>'
-          + '</div>'
-        + '</div>'
-        + '<div class="paragraph chorus">'
-          + '<div class="row">'
-            + '<div class="comment">Breakdown</div>'
-          + '</div>'
-          + '<div class="row">'
-            + '<div class="column">'
-              + '<div class="chord">Em</div>'
-              + '<div class="lyrics">Whisper words of </div>'
-            + '</div>'
-            + '<div class="column">'
-              + '<div class="chord">F</div>'
-              + '<div class="lyrics">wisdom, let it </div>'
-            + '</div>'
-            + '<div class="column">'
-              + '<div class="chord">C</div>'
-              + '<div class="lyrics">be </div>'
-            + '</div>'
-            + '<div class="column">'
-              + '<div class="chord">G</div>'
-              + '<div class="lyrics"></div>'
-            + '</div>'
-          + '</div>'
-        + '</div>'
-      + '</div>';
+    const expectedChordSheet = stripHTML(`
+      <h1>Let it be</h1>
+      <h2>ChordSheetJS example version</h2>
+      <div class="chord-sheet">
+        <div class="paragraph">
+          <div class="row">
+            <div class="column">
+              <div class="chord"></div>
+              <div class="lyrics">Written by: </div>
+            </div>
+            <div class="column">
+              <div class="chord"></div>
+              <div class="lyrics">John Lennon,Paul McCartney</div>
+            </div>
+          </div>
+        </div>
+        <div class="paragraph verse">
+          <div class="row">
+            <h3 class="label">Verse 1</h3>
+          </div>
+          <div class="row">
+            <div class="column">
+              <div class="chord"></div>
+              <div class="lyrics">Let it </div>
+            </div>
+            <div class="column">
+              <div class="chord">Am</div>
+              <div class="lyrics">be, let it </div>
+            </div>
+            <div class="column">
+              <div class="chord">C/G</div>
+              <div class="lyrics">be, let it </div>
+            </div>
+            <div class="column">
+              <div class="chord">F</div>
+              <div class="lyrics">be, let it </div>
+            </div>
+            <div class="column">
+              <div class="chord">C</div>
+              <div class="lyrics">be</div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="column">
+              <div class="chord">D</div>
+              <div class="lyrics">Whisper words of </div>
+            </div>
+            <div class="column">
+              <div class="chord">G</div>
+              <div class="lyrics">wis</div>
+            </div>
+            <div class="column">
+              <div class="chord">A</div>
+              <div class="lyrics">dom, let it </div>
+            </div>
+            <div class="column">
+              <div class="chord">G</div>
+              <div class="lyrics">be </div>
+            </div>
+            <div class="column">
+              <div class="chord">D/F#</div>
+              <div class="lyrics"> </div>
+            </div>
+            <div class="column">
+              <div class="chord">Em</div>
+              <div class="lyrics"> </div>
+            </div>
+            <div class="column">
+              <div class="chord">D</div>
+              <div class="lyrics"></div>
+            </div>
+          </div>
+        </div>
+        <div class="paragraph chorus">
+          <div class="row">
+            <div class="comment">Breakdown</div>
+          </div>
+          <div class="row">
+            <div class="column">
+              <div class="chord">Em</div>
+              <div class="lyrics">Whisper words of </div>
+            </div>
+            <div class="column">
+              <div class="chord">F</div>
+              <div class="lyrics">wisdom, let it </div>
+            </div>
+            <div class="column">
+              <div class="chord">C</div>
+              <div class="lyrics">be </div>
+            </div>
+            <div class="column">
+              <div class="chord">G</div>
+              <div class="lyrics"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `);
 
     expect(formatter.format(song)).toEqual(expectedChordSheet);
   });
@@ -122,24 +125,26 @@ describe('HtmlDivFormatter', () => {
         ],
       ]);
 
-      const expectedChordSheet = '<div class="chord-sheet">'
-          + '<div class="paragraph">'
-            + '<div class="row">'
-              + '<div class="column">'
-                + '<div class="chord">C</div>'
-                + '<div class="lyrics">Whisper words of wisdom</div>'
-              + '</div>'
-            + '</div>'
-          + '</div>'
-          + '<div class="paragraph">'
-            + '<div class="row">'
-              + '<div class="column">'
-                + '<div class="chord">Am</div>'
-                + '<div class="lyrics">Whisper words of wisdom</div>'
-              + '</div>'
-            + '</div>'
-          + '</div>'
-        + '</div>';
+      const expectedChordSheet = stripHTML(`
+        <div class="chord-sheet">
+          <div class="paragraph">
+            <div class="row">
+              <div class="column">
+                <div class="chord">C</div>
+                <div class="lyrics">Whisper words of wisdom</div>
+              </div>
+            </div>
+          </div>
+          <div class="paragraph">
+            <div class="row">
+              <div class="column">
+                <div class="chord">Am</div>
+                <div class="lyrics">Whisper words of wisdom</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      `);
 
       const formatter = new HtmlDivFormatter({ renderBlankLines: false });
 
