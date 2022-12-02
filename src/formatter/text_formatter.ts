4778f8ad40dd3987507dc91fe82d90f3f4047109
Martijn Versluis <martijnversluis@users.noreply.github.com>
2022-12-02 18:00:31 +0100

    Allow passing rendering key to formatters
    This allows you to render a song in any key without transposing it.
    
    For example:
    
    Having a chordpro source:
    
    ```
    {key: C}
    Let it [Am]be let it [C/G]be
    ```
    
    and rendering it with `{ key: 'Eb' }`:
    
    ```
    const song = new ChordProParser().parse(chordpro);
    const formatted = new TextFormatter({ key: 'Eb' });
    ```
    
    Results in:
    
    ```
           Cm        Eb/Bb
    Let it be let it be
    ```
    
    Related to #216

--- src/formatter/text_formatter.ts
+++ src/formatter/text_formatter.ts
@@ -93,7 +93,7 @@ class TextFormatter extends Formatter {
   }
 
   chordLyricsPairLength(chordLyricsPair: ChordLyricsPair, line: Line): number {
-    const chords = renderChord(chordLyricsPair.chords, line, this.song);
+    const chords = renderChord(chordLyricsPair.chords, line, this.song, this.configuration.key);
     const { lyrics } = chordLyricsPair;
     const chordsLength = (chords || '').length;
     const lyricsLength = (lyrics || '').length;
@@ -111,7 +111,7 @@ class TextFormatter extends Formatter {
     }
 
     if (item instanceof ChordLyricsPair) {
-      const chords = renderChord(item.chords, line, this.song);
+      const chords = renderChord(item.chords, line, this.song, this.configuration.key);
       return padLeft(chords, this.chordLyricsPairLength(item, line));
     }
 
--- test/helpers.test.ts
+++ test/helpers.test.ts
@@ -1,6 +1,7 @@
 import Song from '../src/chord_sheet/song';
 import { createLine } from './utilities';
 import { renderChord } from '../src/helpers';
+import Key from '../src/key';
 
 describe('renderChord', () => {
   it('correctly normalizes when a capo is set', () => {
@@ -10,5 +11,13 @@ describe('renderChord', () => {
     song.setMetadata('capo', '1');
 
     expect(renderChord('Dm7', line, song)).toEqual('C#m7');
+  });
+
+  it('can render in a different key', () => {
+    const line = createLine();
+    const song = new Song();
+    song.setMetadata('key', 'F');
+
+    expect(renderChord('Dm7', line, song, Key.parse('B'))).toEqual('G#m7');
   });
 });
