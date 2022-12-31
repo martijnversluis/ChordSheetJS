bd2f3be3b16a28de22991e431200a858d70f46f3
Martijn Versluis <martijnversluis@users.noreply.github.com>
2022-12-31 14:25:56 +0100

    Add font styling to empty divs (#790)

--- test/integration/chord_pro_to_html_div.test.ts
+++ test/integration/chord_pro_to_html_div.test.ts
@@ -70,7 +70,7 @@ describe('chordpro to HTML with DIVs', () => {
     expect(formatted).toEqual('<div class="chord-sheet"></div>');
   });
 
-  it('renders style attributes for chord font, size and color', () => {
+  it('renders style attributes for chord/text font, size and color', () => {
     const chordProSheet = `
 {chordfont: "Times New Roman"}
 {chordfont: sans-serif}
@@ -87,7 +87,9 @@ describe('chordpro to HTML with DIVs', () => {
 {chordsize}
 {chordcolour}
 {chordfont}
-[Dm]12px red "Times New Roman"
+{textcolour: green}
+[Am][Dm]12px red "Times New Roman"
+{textcolour}
 {chordsize}
 {chordcolour}
 {chordfont}
@@ -102,7 +104,7 @@ describe('chordpro to HTML with DIVs', () => {
               <div class="lyrics">30px </div>
             </div>
             <div class="column">
-              <div class="chord"></div>
+              <div class="chord" style="color: green; font: 30px sans-serif"></div>
               <div class="lyrics">green sans-serif</div>
             </div>
           </div>
@@ -112,18 +114,22 @@ describe('chordpro to HTML with DIVs', () => {
               <div class="lyrics">24px </div>
             </div>
             <div class="column">
-              <div class="chord"></div>
+              <div class="chord" style="color: blue; font: 24px sans-serif"></div>
               <div class="lyrics">blue sans-serif</div>
             </div>
           </div>
           <div class="row">
             <div class="column">
-              <div class="chord" style="color: red; font: 12px 'Times New Roman'">Dm</div>
-              <div class="lyrics">12px </div>
+              <div class="chord" style="color: red; font: 12px 'Times New Roman'">Am</div>
+              <div class="lyrics" style="color: green"></div>
             </div>
             <div class="column">
-              <div class="chord"></div>
-              <div class="lyrics">red "Times New Roman"</div>
+              <div class="chord" style="color: red; font: 12px 'Times New Roman'">Dm</div>
+              <div class="lyrics" style="color: green">12px </div>
+            </div>
+            <div class="column">
+              <div class="chord" style="color: red; font: 12px 'Times New Roman'"></div>
+              <div class="lyrics" style="color: green">red "Times New Roman"</div>
             </div>
           </div>
           <div class="row">
