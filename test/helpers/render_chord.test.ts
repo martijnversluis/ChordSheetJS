fa2669efda424e9ef754c88f045c2fe11ea73614
Samuele Mazzi <samuele.mazzi@minervasys.tech>
2023-12-30 12:17:07 +0100

    fix whitespaces

--- test/helpers/render_chord.test.ts
+++ test/helpers/render_chord.test.ts
@@ -9,15 +9,15 @@ describe('renderChord helper', () => {
     eachTestCase(`
       #  | songKey | capo | lineKey | lineTransposeKey | renderKey | outcome |
       -- | ------- | ---- | ------- | ---------------- | --------- | ------- |
-        1 |         |      |         |                  |           | "Em7"   |
-        2 |         |      |         |                  | "F"       | "Em7"   |
-        3 |         |      |         | "A"              |           | "Em7"   |
-        4 |         |      |         | "A"              | "F"       | "Em7"   |
-        5 |         |      | "Bb"    |                  | "F"       | "Em7"   |
-        6 |         |      | "Bb"    | "A"              | "F"       | "Em7"   |
-        7 |         | 3    |         |                  |           | "Dbm7"  |
-        8 |         | 3    |         |                  | "F"       | "Dbm7"  |
-        9 |         | 3    |         | "A"              |           | "Dbm7"  |
+       1 |         |      |         |                  |           | "Em7"   |
+       2 |         |      |         |                  | "F"       | "Em7"   |
+       3 |         |      |         | "A"              |           | "Em7"   |
+       4 |         |      |         | "A"              | "F"       | "Em7"   |
+       5 |         |      | "Bb"    |                  | "F"       | "Em7"   |
+       6 |         |      | "Bb"    | "A"              | "F"       | "Em7"   |
+       7 |         | 3    |         |                  |           | "Dbm7"  |
+       8 |         | 3    |         |                  | "F"       | "Dbm7"  |
+       9 |         | 3    |         | "A"              |           | "Dbm7"  |
       10 |         | 3    |         | "A"              | "F"       | "Dbm7"  |
       11 |         | 3    | "Bb"    |                  |           | "Dbm7"  |
       12 |         | 3    | "Bb"    |                  | "F"       | "Dbm7"  |
@@ -105,15 +105,15 @@ describe('renderChord helper', () => {
     eachTestCase(`
       #  | chordString | songKey | chordStyle | outcome |
       -- | ----------- | ------- | ---------- | ------- |
-        1 | "Gm"        |         | "symbol"   | "Gm"    |
-        2 | "Gm"        | "Bb"    | "symbol"   | "Gm"    |
-        s | "Gm"        | "Bb"    | "number"   | "6"     |
-        s | "Gm"        | "Bb"    | "numeral"  | "vi"    |
-        5 | "IV"        |         | "numeral"  | "IV"    |
-        6 | "IV"        |         | "number"   | "4"     |
-        7 | "IV"        | "Bb"    | "symbol"   | "Eb"    |
-        8 | "IV"        | "Bb"    | "numeral"  | "IV"    | 
-        9 | "IV"        | "Bb"    | "number"   | "4"     |
+       1 | "Gm"        |         | "symbol"   | "Gm"    |
+       2 | "Gm"        | "Bb"    | "symbol"   | "Gm"    |
+       s | "Gm"        | "Bb"    | "number"   | "6"     |
+       s | "Gm"        | "Bb"    | "numeral"  | "vi"    |
+       5 | "IV"        |         | "numeral"  | "IV"    |
+       6 | "IV"        |         | "number"   | "4"     |
+       7 | "IV"        | "Bb"    | "symbol"   | "Eb"    |
+       8 | "IV"        | "Bb"    | "numeral"  | "IV"    | 
+       9 | "IV"        | "Bb"    | "number"   | "4"     |
       10 | "4"         |         | "numeral"  | "IV"    |
       11 | "4"         |         | "number"   | "4"     |
       12 | "4"         | "Bb"    | "symbol"   | "Eb"    |
