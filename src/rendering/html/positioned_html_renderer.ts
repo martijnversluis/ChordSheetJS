d0fc804364d88b3779099def334de1670485572a
isaiahdahl <isaiahdahl@me.com>
2026-07-27 14:02:31 -0700

    Add configurable chord extension superscripts

--- src/rendering/html/positioned_html_renderer.ts
+++ src/rendering/html/positioned_html_renderer.ts
@@ -9,6 +9,7 @@ import Tag from '../../chord_sheet/tag';
 import { getCapos } from '../../helpers';
 import { isChordTokenKind } from '../../chord_sheet/chord_line_token';
 import { isComment } from '../../template_helpers';
+import renderChordRuns from './chord_run_renderer';
 import { LineLayout, MeasuredItem } from '../../layout/engine';
 
 import LayoutSectionRenderer, { LayoutRenderingBackend } from '../shared/layout_section_renderer';
@@ -382,7 +383,7 @@ class PositionedHtmlRenderer extends Renderer {
     if (chords && isChordTokenKind(item.tokenKind)) chords = this.processChords(chords, line);
 
     if (!this.isLyricsOnly() && chords) {
-      const chordBaseline = this.calculateChordBaseline(chordsYOffset, items, chords);
+      const chordBaseline = this.getChordBaseline(item, chordsYOffset, items, chords);
       this.addChordLineToken(item, chords, currentX, chordBaseline);
       this.updatePosition(ctx.column, ctx.page);
     }
@@ -461,12 +462,6 @@ class PositionedHtmlRenderer extends Renderer {
   protected measureText(text: string, font: FontConfiguration): { width: number; height: number } {
     const { w, h } = this.doc.getTextDimensions(text, font);
     return { width: w, height: h };
-  }
-
-  protected calculateChordBaseline(yOffset: number, items: MeasuredItem[], chordText: string): number {
-    const chordFont = this.getFontConfiguration('chord');
-    const { height } = this.measureText(chordText, chordFont);
-    return yOffset + this.getMaxChordHeight(items) - height;
   }
 
   protected finalizeRendering(): void {
@@ -624,6 +619,7 @@ class PositionedHtmlRenderer extends Renderer {
       ],
     );
 
+    renderChordRuns(htmlElement, element, this.configuration, this.styler, this.useUnicodeModifiers());
     this.styler.applyElementStyle(htmlElement, element);
     paragraphDiv.appendChild(htmlElement);
   }
@@ -663,6 +659,7 @@ class PositionedHtmlRenderer extends Renderer {
     }
 
     htmlElement.textContent = element.content;
+    renderChordRuns(htmlElement, element, this.configuration, this.styler, this.useUnicodeModifiers());
 
     if (element.style) {
       this.styler.applyElementStyle(htmlElement, element);
